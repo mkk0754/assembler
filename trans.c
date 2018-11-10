45e972ebf30c299256e10594cc60c81992e4b222
mkk0754 <rkdalsrb1212@naver.com>
2018-11-11 06:18:12 +0900

    trans.c in make move little

--- trans.c
+++ trans.c
@@ -16,6 +16,45 @@ int instr_trans(char *op, char *args, char* mcode)
 	/*    generate machine code     */
     /*  							*/
 	/********************************/
+	switch(op){
+	case "mov":move(args,mcode);break;
+	case "push":break;
+	case "add":break;
+	case "pop":break;
+	case "sub":break;
+	case "call":break;
+	default:break;
+	}
 	
 	return 1;	
+}
+int move(char *args, char *mcode){
+	char left[255] ={ NULL };
+	char *right[255] ={ NULL };
+	char *ptr_l[255] ={ NULL };
+	char *ptr_r[255] ={ NULL };
+
+	left = strtok(args, ",");
+	strcpy(right,args);
+	
+	strcpy(ptr_l,left);
+	strcpy(ptr_r,right);
+	if(ptr_l = strchr(ptr_l,'-') || ptr_l = strchr(ptr_l,'(')){
+	strcpy(mcode, "8b");
+	}
+	else if(ptr_l = strchr(ptr_l, '%')){
+		if(ptr_r = strchr(ptr_r, '%'))
+			strcpy(mcode, "89");
+		else
+			strcpy(mcode, "a3");
+	}
+	else if(ptr_l = strchr(ptr_l,'$')){
+		
+	}
+	else{
+		strcpy(mcode, "a1");
+	}
+	strcpy(mcode, "b8");
+	
+	return 0;	
 }
