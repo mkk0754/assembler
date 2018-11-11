a77e81321bfeaec51c80282f592e83a4dda3f0d0
mkk0754 <rkdalsrb1212@naver.com>
2018-11-11 11:29:03 +0900

    update check.c

--- check.c
+++ check.c
@@ -2,6 +2,10 @@
 
 int is_valid(char *op, char *args)
 {
+	char left[255] = {NULL};
+	char right[255] = {NULL};
+	int left_lexem;
+	int left_lexem;
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
@@ -10,6 +14,18 @@ int is_valid(char *op, char *args)
 	/*         syntax check         */
     /*  							*/
 	/********************************/
-	
+	left = strtok(args,",");
+	strcpy(right,args);
+	left_lexem = lex(left);
+	right_lexem = lex(right);
+	if(left == mem && right == mem){
+		return 0;
+	}
+	else if(left == immediate && right == immediate){
+		return 0;
+	}
+	else if(right == immeditate){
+		return 0;
+	}
 	return 1;
 }
